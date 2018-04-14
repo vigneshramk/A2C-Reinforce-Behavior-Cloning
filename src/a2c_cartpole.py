9fa4a5ddcb3ac952489fc1e5533a32bc8194b9ec
vigneshramk <ram7sachin@gmail.com>
2018-04-14 11:39:52 -0400

    A2C Cartpole added
    Signed-off-by: vigneshramk <ram7sachin@gmail.com>

--- src/a2c_cartpole.py
+++ src/a2c_cartpole.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import gym
 import os
+from copy import copy, deepcopy
 
 import matplotlib
 matplotlib.use('Agg')
@@ -16,6 +17,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.distributions import Categorical
+
+# Selecting the gpu
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
 
 def np_to_variable(x, requires_grad=False, is_cuda=True, dtype=torch.FloatTensor):
     v = Variable(torch.from_numpy(x).type(dtype), requires_grad=requires_grad)
@@ -42,18 +47,11 @@ class Policy(nn.Module):
         self.classifier = nn.Sequential(
                           nn.Linear(state_size, self.hidden_size),
                           nn.Tanh(),
-                          nn.Linear(self.hidden_size, self.hidden_size*2),
+                          nn.Linear(self.hidden_size, self.hidden_size),
                           nn.Tanh(),
-                          nn.Linear(self.hidden_size*2, action_size))
-
-        # self.classifier = nn.Sequential(
-        #                   nn.Linear(state_size, self.hidden_size),
-        #                   nn.ReLU(inplace=True),
-        #                   nn.Linear(self.hidden_size, self.hidden_size),
-        #                   nn.ReLU(inplace=True),
-        #                   nn.Linear(self.hidden_size, self.hidden_size),
-        #                   nn.ReLU(inplace=True),
-        #                   nn.Linear(self.hidden_size, action_size))
+              nn.Linear(self.hidden_size, self.hidden_size),
+                          nn.Tanh(),
+                          nn.Linear(self.hidden_size, action_size))
 
     def forward(self, x):
         x = self.classifier(x)
@@ -66,16 +64,14 @@ class Value(nn.Module):
         self.hidden_size = 16
         self.classifier = nn.Sequential(
                           nn.Linear(state_size, self.hidden_size),
-                          nn.Tanh(),
-                          nn.Linear(self.hidden_size, self.hidden_size),
-                          nn.Tanh(),
-                          nn.Linear(self.hidden_size, 1))
-        # self.classifier = nn.Sequential(
-        #                   nn.Linear(state_size, self.hidden_size),
-        #                   nn.ReLU(inplace=True),
-        #                   nn.Linear(self.hidden_size, self.hidden_size),
-        #                   nn.ReLU(inplace=True),
-        #                   nn.Linear(self.hidden_size, 1))
+                          nn.ReLU(),
+                          nn.Linear(self.hidden_size, self.hidden_size*4),
+                          nn.ReLU(),
+                          nn.Linear(self.hidden_size*4, self.hidden_size*16),
+                          nn.ReLU(),
+                          nn.Linear(self.hidden_size*16, self.hidden_size*4),
+                          nn.ReLU(),
+                          nn.Linear(self.hidden_size*4, 1))
 
     def forward(self, x):
         x = self.classifier(x)
@@ -86,7 +82,7 @@ class A2C(Reinforce):
     # This class inherits the Reinforce class, so for example, you can reuse
     # generate_episode() here.
 
-    def __init__(self, model, lr, critic_model, critic_lr, n=20):
+    def __init__(self, actor_model, lr, critic_model, critic_lr, n=20):
         # Initializes A2C.
         # Args:
         # - model: The actor model.
@@ -94,28 +90,26 @@ class A2C(Reinforce):
         # - critic_model: The critic model.
         # - critic_lr: Learning rate for the critic model.
         # - n: The value of N in N-step A2C.
-        self.model = model
-        self.critic_model = critic_model
+        self.actor_model = deepcopy(actor_model)
+        self.critic_model = deepcopy(critic_model)
         self.n = n
 
         # TODO: Define any training operations and optimizers here, initialize
         #       your variables, or alternately compile your model here.
 
-        self.optimizer_actor = optim.Adam(model.parameters(), lr=lr)
+        self.optimizer_actor = optim.Adam(self.actor_model.parameters(), lr=lr)
+        self.optimizer_critic = optim.Adam(self.critic_model.parameters(), lr=critic_lr)
+        self.loss_critic = nn.MSELoss()
 
-        self.optimizer_critic = optim.Adam(critic_model.parameters(), lr=critic_lr)
 
-        print('Finished initializing')
+        print('Finished initializing') 
 
-    def entropy(self, p):
-        return -torch.sum(p * torch.log(p), 1)  
-
-    def train(self, env, gamma=1.0):
+    def train(self, env, gamma=0.99):
         # Trains the model on a single episode using A2C.
         # TODO: Implement this method. It may be helpful to call the class
         #       method generate_episode() to generate training data.
 
-        states,actions,rewards, log_probs = self.generate_episode(env)
+        states,actions,rewards, log_probs = self.generate_episode(self.actor_model, env)
 
         states = np.array(states)
         actions = np.array(actions)
@@ -126,10 +120,14 @@ class A2C(Reinforce):
         N_vec = range(self.n)
         gamma_vec = [pow(gamma,n) for n in N_vec]
         V_vec = np.zeros(T)
+        V_vec_th_critic = []
 
         for t in range(T):
-            s_curr = np_to_variable(states[t], requires_grad=True)
-            V_vec[t] = self.critic_model(s_curr).data.cpu().numpy()[0]
+            s_curr = np_to_variable(states[t], requires_grad=False)
+            V_vec_th_critic.append(self.critic_model(s_curr))
+            V_vec[t] = V_vec_th_critic[t].data.cpu().numpy()[0]
+
+        V_vec_th_critic = torch.cat(V_vec_th_critic)
         
         # Compute the N-step rewards
         for t in range(T)[::-1]:
@@ -145,35 +143,43 @@ class A2C(Reinforce):
 
         #Cum rewards vector for each episode
         R = np_to_variable(R, requires_grad=False)
-        V_vec = np_to_variable(V_vec, requires_grad=True)
-        cum_R = R - V_vec
+        V_vec_th_actor = np_to_variable(V_vec, requires_grad=False)
+        cum_R = R - V_vec_th_actor
 
         #Loss definition for the actor and critic networks
         #Actor
 
         # Actor update
         hadamard_prod = []
-        # entropy_loss = []
         for l_prob, c_R in zip(log_probs, cum_R):
             hadamard_prod.append(-l_prob * c_R)
-            # entropy_loss.append(torch.mean(self.entropy(torch.exp(l_prob))))
 
-        # log_probs_th = torch.FloatTensor(log_probs)
-        # entropy_loss = torch.mean(self.entropy(torch.exp(log_probs_th)))
         self.optimizer_actor.zero_grad()
         loss_actor = torch.mean(torch.cat(hadamard_prod))
         loss_actor.backward()
+        nn.utils.clip_grad_norm(self.actor_model.parameters(), 1)
         self.optimizer_actor.step()
 
         # Critic update
-        loss_critic = nn.MSELoss()
-        loss_th_critic = loss_critic(V_vec,R)
-
         self.optimizer_critic.zero_grad()
-        (-loss_th_critic).backward()
+        loss_th_critic = self.loss_critic(V_vec_th_critic,R)
+        loss_th_critic.backward()
+        nn.utils.clip_grad_norm(self.critic_model.parameters(), 1)
         self.optimizer_critic.step()
 
         return np.sum(rewards), loss_actor, loss_th_critic.data[0]
+
+    def test(self, env, num_episodes = 100,model_file=None):
+        
+        reward_epi = []
+        for i in range(num_episodes):
+
+            states,actions,rewards,log_probs = self.generate_episode(self.actor_model, env)
+            reward_epi.append(np.sum(rewards))
+
+        reward_epi = np.array(reward_epi)
+
+        return np.mean(reward_epi), np.std(reward_epi)
 
 def parse_arguments():
     # Command-line flags are defined here.
@@ -188,7 +194,7 @@ def parse_arguments():
     parser.add_argument('--critic-lr', dest='critic_lr', type=float,
                         default=1e-4, help="The critic's learning rate.")
     parser.add_argument('--n', dest='n', type=int,
-                        default=20, help="The value of N in N-step A2C.")
+                        default=100, help="The value of N in N-step A2C.")
 
     # https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
     parser_group = parser.add_mutually_exclusive_group(required=False)
@@ -214,7 +220,6 @@ def main(args):
     render = args.render
 
     # Create the environment.
-    # env = gym.make('LunarLander-v2')
     env = gym.make('CartPole-v0')
 
     # Set the seeds
@@ -222,13 +227,8 @@ def main(args):
     np.random.seed(2018)
     env.seed(2018)
 
-    # Set the seeds
-    torch.manual_seed(2018)
-    np.random.seed(2018)
-    env.seed(2018)
-
-    num_episodes = 10000
-    gamma = 0.95
+    num_episodes = 50000
+    gamma = 0.99
     print(env.observation_space.shape)
     print(env.action_space)
     state_size = env.observation_space.shape[0]
@@ -236,16 +236,31 @@ def main(args):
     print("State_size:{}, Action_size{}".format(state_size, action_size))
 
     #Define actor and crtiic learning rates here
-    actor_lr = 0.001
-    critic_lr = 0.001
+    actor_lr = 5e-4
+    critic_lr = 5e-4
+
+    # # Create plot
+    # fig1 = plt.figure()
+    # ax1 = fig1.gca()
+    # ax1.set_title('Per episode Cum. Return Plot')
+    path_name = './fig_a2c_cp_n100'
+    if not os.path.exists(path_name):
+        os.makedirs(path_name)
+    # plot1_name = os.path.join(path_name,'reinforce_discounted_reward.png')
+    # plot1_name = os.path.join(path_name,'a2c_reward.png')
 
     # Create plot
     fig1 = plt.figure()
     ax1 = fig1.gca()
     ax1.set_title('Per episode Cum. Return Plot')
-    path_name = './fig_a2c'
-    # plot1_name = os.path.join(path_name,'reinforce_discounted_reward.png')
-    plot1_name = os.path.join(path_name,'a2c_reward.png')
+
+    fig2 = plt.figure()
+    ax2 = fig2.gca()
+    ax2.set_title('Test Reward Plot')
+   
+    fig3 = plt.figure()
+    ax3 = fig3.gca()
+    ax3.set_title('Test Reward Plot')
 
     # TODO: Train the model using A2C and plot the learning curves.
 
@@ -262,18 +277,31 @@ def main(args):
     for i in range(num_episodes):
         cum_reward, loss_actor, loss_critic = a2cAgent.train(env,gamma)
         cum_reward *= 100
+        # plt.pause(0.005)
+        if i%100 == 0:
+            print("Rewards for episode %s is %1.2f" %(i,cum_reward))
+            # print("Loss Actor for episode %s is %1.2f" %(i,loss_actor))
+            # print("Loss Critic for episode %s is %1.2f" %(i,loss_critic))
+
+        #Test every 300 episodes
+        if i % 100 == 0:
+            mean_r, std_r = a2cAgent.test(env)
+
+            print('Episode %s - Mean - %1.2f  Std - %1.2f' %(i,mean_r,std_r))
+            ax2.errorbar(i+1, mean_r, yerr=std_r, fmt='o')
+            ax3.errorbar(i+1, mean_r+20, yerr=std_r, fmt='o')   
         # Plot the discounted reward per episode
         ax1.scatter(i, cum_reward)
-        plt.pause(0.005)
         if i%100 == 0:
+            str_path1 = 'a2c_training_reward' + str(i) + '.png'
+            str_path2 = 'a2c_test_reward' + str(i) + '.png'
+            str_path3 = 'a2c_testfinal_reward' + str(i) + '.png'
+            plot1_name = os.path.join(path_name,str_path1)
+            plot2_name = os.path.join(path_name,str_path2)
+            plot3_name = os.path.join(path_name,str_path3)
             ax1.figure.savefig(plot1_name)
-            print("Rewards for episode %s is %1.2f" %(i,cum_reward))
-            print("Loss Actor for episode %s is %1.2f" %(i,loss_actor))
-            print("Loss Critic for episode %s is %1.2f" %(i,loss_critic))
-
-
-
-
+            ax2.figure.savefig(plot2_name)
+            ax3.figure.savefig(plot3_name)
 
 if __name__ == '__main__':
     main(sys.argv)
